bdb058a4962b1c25a7c3fa5ad2a4af6515122e22
zhengbowen01 <zhengbowen01@MacBook-Pro.local>
2022-10-13 10:49:18 +0800

    bug demo

--- src/index.js
+++ src/index.js
@@ -1,17 +1,56 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { useState } from "react";
+import { createRoot } from "react-dom/client";
+import { useRequest } from "ahooks";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+let parentData = 0;
+let childData = 0;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function fetchDataForParent() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(parentData++);
+    }, 100);
+  });
+}
+
+function fetchDataForChild() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(childData++);
+    }, 100);
+  });
+}
+
+function Parent() {
+  const { data } = useRequest(() => fetchDataForParent(), {
+    refreshOnWindowFocus: true,
+    focusTimespan: 0,
+  });
+  const [isChildVisible, setIsChildVisible] = useState(true);
+  return (
+    <div>
+      <div>I am parent</div>
+      <div>parent data: {data}</div>
+      {isChildVisible && <Child />}
+      <button onClick={() => setIsChildVisible(!isChildVisible)}>
+        switch child visible
+      </button>
+    </div>
+  );
+}
+
+function Child() {
+  const { data } = useRequest(() => fetchDataForChild(), {
+    refreshOnWindowFocus: true,
+    focusTimespan: 0,
+  });
+  return (
+    <div>
+      I am child
+      <div>child data: {data}</div>
+    </div>
+  );
+}
+
+const root = createRoot(document.getElementById("root"));
+root.render(<Parent />);
